5c6068b47d711c4d2d106d648668c8aa62c06d5d
AtsushiUtsumi <debtgx103@gmail.com>
2025-04-23 18:33:45 +0900

    引数で育成パラメータ取得するように変更

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ from time import sleep
 import pyautogui
 # 画面のボタンの座標は「.env」に移すこと
 # ボタンの押下はメソッド化してテストしやすくすること
+
+mode = 'C'
+count = 10# 何回育成するか
+primary_param_index = 0# メインステータスのインデックス
+secondary_param_index = 3# サブステータスのインデックス
+
+import sys
+args = sys.argv
+
+if len(args) != 4:
+    print("引数の数が違います")
+    print("引数1: B or C")
+    print("引数2: 育成回数")
+    print("引数3: メインステータスのインデックス")
+    exit(0)
+mode = args[1]
+count = int(args[2])
+primary_param_index = int(args[3])
+
 def press_button_b():
     print("B級育成")
     pyautogui.moveTo(1000, 770, duration=0.2)# B級育成ボタン
@@ -21,18 +40,18 @@ def press_button_save():
     pyautogui.moveTo(1060, 770, duration=0.2)# 保存
     pyautogui.click()
 
-count = 10# 何回育成するか
 save_count = 0
 not_save_count = 0
 change_list = [0, 0, 0, 0]# ステータスの変化量を記録するリスト
 
 for i in range(count):# 100個使っても0.32%しか上昇しないので注意
     print("\n======================< " + str(i + 1) + "/" + str(count) + " >======================")
-    press_button_c()
+    if mode == "B":
+        press_button_b()
+    elif mode == "C":
+        press_button_c()
     param_list = get_param_list()
     print(param_list)
-    primary_param_index = 0# メインステータスのインデックス
-    secondary_param_index = 3# サブステータスのインデックス
     if param_list[primary_param_index] + param_list[secondary_param_index] == 0 and param_list[primary_param_index] > param_list[secondary_param_index]:# 合計がでもメインステータスがプラスなら保存
         print("保存(メインステータス優先)")
         save_count += 1
