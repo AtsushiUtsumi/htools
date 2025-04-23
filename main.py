60d53c38fe370b551b5335b9ada9c8987d4be99b
AtsushiUtsumi <debtgx103@gmail.com>
2025-04-23 15:44:54 +0900

    変化量記録

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ def press_button_save():
     pyautogui.moveTo(1060, 770, duration=0.2)# 保存
     pyautogui.click()
 
-count = 100# 何回育成するか
+count = 1000# 何回育成するか
 save_count = 0
 not_save_count = 0
+change_list = [0, 0, 0, 0]# ステータスの変化量を記録するリスト
 
 for i in range(count):# 100個使っても0.32%しか上昇しないので注意
     print("\n======================< " + str(i + 1) + "/" + str(count) + " >======================")
@@ -35,13 +36,16 @@ for i in range(count):# 100個使っても0.32%しか上昇しないので注意
     if param_list[primary_param_index] + param_list[secondary_param_index] == 0 and param_list[primary_param_index] > param_list[secondary_param_index]:# 合計がでもメインステータスがプラスなら保存
         print("保存(メインステータス優先)")
         save_count += 1
+        for i in range(len(param_list)):change_list[i] += param_list[i]
         press_button_save()
     elif param_list[primary_param_index] + param_list[secondary_param_index] > 0:# 合計が正なら保存
         print("保存")
         save_count += 1
+        for i in range(len(param_list)):change_list[i] += param_list[i]
         press_button_save()
     else:
         print("やめる")
         not_save_count += 1
         press_button_not_save()
     print("保存回数: " + str(save_count) + " / やめた回数: " + str(not_save_count) + " / 合計: " + str(save_count + not_save_count))
+    print("ステータスの変化量: " + str(change_list))
